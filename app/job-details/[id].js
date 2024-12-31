131dd316154577ce6e14fd9d07300d7307c0eb5b
MadhuKumarGowda <madhukumar.gowda@gmail.com>
2024-12-31 16:07:11 -0500

    added job details page with router navigation

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -24,6 +24,7 @@ import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
 import styles from "../../components/common/header/screenheader.style";
 
+const tabs = ["About", "Qualifications", "Responsbilities"];
 const JobDetails = () => {
   const params = useGlobalSearchParams();
 
@@ -35,8 +36,34 @@ const JobDetails = () => {
   });
 
   const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
 
   const onRefresh = () => {};
+
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return (
+          <Specifics
+            title="Qulaifications"
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+      case "About":
+        return (
+          <JobAbout info={data[0].job_description ?? "No Data provided"} />
+        );
+      case "Responsbilities":
+        return (
+          <Specifics
+            title="Responsibilities"
+            points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+          />
+        );
+      default:
+        break;
+    }
+  };
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -72,11 +99,25 @@ const JobDetails = () => {
             <Text> No Data</Text>
           ) : (
             <View style={{ padidng: SIZES.medium, paddingBottom: 100 }}>
-              <Company />
-              <JobTabs />
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
+        <JobFooter
+          url={data[0]?.job_google_link ?? "https://google.com/jobs/results"}
+        />
       </>
     </SafeAreaView>
   );
--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 
 import styles from './company.style'
+import { icons } from '../../../constants'
+import { checkImageURL } from '../../../utils'
 
-const Company = () => {
+const Company = ({ companrLogo, jobTitle, companyName, location}) => {
   return (
-    <View>
-      <Text>Company</Text>
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+      <Image source={{uri:companrLogo}} style={styles.logoImage} />
+      </View>
+      <View style={styles.jobTitleBox} >
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}> {companyName}</Text>
+        <View style={styles.locationBox}>
+          <Image source={icons.location} resizeMode='"conatin' style={styles.locationImage} />
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
+      </View>
     </View>
   )
 }
